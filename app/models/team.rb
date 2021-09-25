2f09a22123ee593b5446c1ead0683c1526f22569
”HIDEKIHIDEKI”HIDEKI <chanike1993@yahoo.co.jp>
2021-09-25 17:51:07 +0900

    アジェンダ削除機能追加

--- app/models/team.rb
+++ app/models/team.rb
@@ -16,4 +16,12 @@ class Team < ApplicationRecord
     assigns.create(user: user)
   end
 
+  def all_member_emails
+    self.members.map(&:email)
+  end
+
+  def deliver_agenda_destroy_mails
+    AgendaDestroyMailer.deliver_agenda_destroy_mail(self.all_member_emails)
+  end
+
 end
